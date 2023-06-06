df006500b76a3da79d780363eb60521b5162e208
Jacob <Jacobabrooks04@gmail.com>
2023-06-06 15:42:43 +0100

    Second Commit

---
+++ app.js
@@ -0,0 +1,56 @@
+const cors = require("cors")
+const fruits = require('./fruits.json')
+const express = require("express")
+const app = express()
+// const logger = require("./logger")
+// app.use(logger)
+app.use('/fruits',express.json())
+
+app.get('/', (req,res) => {
+    res.send("Hello Fruit API")
+})
+
+app.get('/fruits', (req,res) => {
+    res.send(fruits)
+})
+
+
+app.get('/fruits/:name',(req,res) => {
+    const name = req.params.name.toLowerCase()
+    const fruit = fruits.find((fruit) => fruit.name.toLowerCase() == name)
+    if (fruit == undefined){
+        res.status(404).send()
+    } else {
+        res.send(fruit)
+    }
+})
+
+app.post('/fruits', (req,res) => {
+    //Check if fruit is in Json
+    const fruit = fruits.find((fruit) => fruit.name.toLowerCase() == req.body.name.toLowerCase())
+    if (fruit != undefined){
+        res.status(409).send()
+    } else {
+        fruits.push(req.body)
+        res.status(201).send(req.body)
+    }
+})
+
+app.delete('/fruits/:name', (req,res) => {
+    // see if exists
+    const fruit = fruits.find((fruit) => fruit.name.toLowerCase() == req.params.name.toLowerCase())
+    if (fruit == undefined){
+        res.status(404).send()
+    } else {
+        
+        const indexToDelete = fruits.indexOf(fruit,1)
+        fruits.splice(indexToDelete, 1)
+        res.status(204).send()
+    }})
+
+
+// app.listen(port, () => {
+//     console.log(`Fruity Api listening on port ${port}`)
+// })
+
+module.exports = app 
